2f9e775903d31d7858f741ad0405f75234f30580
mhmddzidane <muhammadzidane633@gmail.com>
2023-02-27 13:28:19 +0700

    add tailwindcss and make header and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import "./App.css";
 import CounterExample from "./components/CounterExample";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import HelloWorld from "./components/HelloWorld";
 
 function App() {
   return (
     <div>
+      <Header />
       <HelloWorld name={"zidane"} />
       <CounterExample />
+      <Footer />
     </div>
   );
 }
